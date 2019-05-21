d35fbb26fbb4c56329497a146eb986335fe9b0dc
ceya <regist.ceya@gmail.com>
2019-05-21 09:37:22 +0900

    基礎デザイン完了

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,9 @@
 
-import Vue from 'vue';
+
+import $ from 'jquery';
 import _ from 'lodash';
+import Popper from 'popper.js';
+import BootStrap from 'bootstrap';
+//import Vue from 'vue';
 
 console.log('test');
